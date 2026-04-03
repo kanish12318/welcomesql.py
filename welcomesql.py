d21281d4a9f5ded22fcc526aa64793b8b8e454a4
kanish12318 <Siyajindal376@gmail.com>
2026-04-03 21:31:25 +0530

    Add files via upload

---
+++ welcomesql.py
@@ -0,0 +1,7 @@
+print("what is the type of above given database?\n (1) Relational Database\n (2) Non-Relational Database")
+answer =int(input("enter your guess: "))
+if answer==2:
+    print("correct")
+else:
+    print("better luck next time")
+print("why you choose this?")
